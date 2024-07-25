5c860e95343a2f9d18bb6a565e74ad00aac3daef
Shirley <shirley.xu.com@gmail.com>
2024-07-25 15:52:40 -0400

    Add cors headers

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,13 +3,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 // To handle a POST request to /api
-export async function GET() {
+export async function GET(request: NextRequest) {
+  request.headers.set("Access-Control-Allow-Origin", "*");
+  request.headers.set("Access-Control-Allow-Methods", "GET");
+  request.headers.set("Access-Control-Allow-Headers", "Content-Type");
   const messages = await prisma.message.findMany();
   return NextResponse.json({ messages });
 }
 
 export async function POST(request: NextRequest) {
   // Do whatever you want
+  request.headers.set("Access-Control-Allow-Origin", "*");
+  request.headers.set("Access-Control-Allow-Methods", "POST");
+  request.headers.set("Access-Control-Allow-Headers", "Content-Type");
   const body = await request.json();
   console.log(body);
 
